b7526a90768126959f046528947a43a315dabf2a
khkssong6949 <bada6949@gmial.com>
2020-06-17 20:06:53 +0900

    자동로그인 체크박스 / 체크 결과 저장 준비

--- app/src/main/java/com/hgney/apipractice_20200615/utils/ContextUtil.kt
+++ app/src/main/java/com/hgney/apipractice_20200615/utils/ContextUtil.kt
@@ -7,6 +7,7 @@ class ContextUtil {
 
         //    메모장(preference) 파일 이름에 대응되는 개념 : 변수로 저장
         val prefName = "APIPracticef"
+        val AUTO_LOGIN = "AUTO_LOGIN"
 
         //    저장할 항목들의 이름을 변수로 생성
         val USER_TOKEN = "USER_TOKEN"
@@ -22,6 +23,16 @@ class ContextUtil {
         fun getUserToken(context: Context): String {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             return pref.getString(USER_TOKEN, "")!!
+        }
+
+        fun setAutoLogin (context: Context, autoLogin : Boolean) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+        }
+
+        fun isAutoLogin (context: Context) : Boolean {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(AUTO_LOGIN, false)
         }
 
     }
