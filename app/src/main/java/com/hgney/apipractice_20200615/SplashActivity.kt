580330c70e5145e7b611814ba5547154bbfed181
khkssong6949 <bada6949@gmial.com>
2020-06-17 20:38:14 +0900

    스플래쉬 화면 생성 => 시작점으로 설정
     - 앱 시작시 자동로그인 가능 여부 검사 준비

---
+++ app/src/main/java/com/hgney/apipractice_20200615/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.hgney.apipractice_20200615
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivitiy() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
