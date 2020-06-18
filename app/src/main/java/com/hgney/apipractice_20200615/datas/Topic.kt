17c8de5a163e650db1c2902e33b7b601869c34b8
khkssong6949 <bada6949@gmial.com>
2020-06-18 19:54:00 +0900

    주제의 내용중 선택진영들 파싱 작업중
     -> 선택 진영도 일종의 클래스화 (파싱 기능 작업)

--- app/src/main/java/com/hgney/apipractice_20200615/datas/Topic.kt
+++ app/src/main/java/com/hgney/apipractice_20200615/datas/Topic.kt
@@ -15,6 +15,12 @@ class Topic {
             topic.title = json.getString("title")
             topic.imageUrl = json.getString("img_url")
 
+//            선택 가능 진영 정보 파싱 => JSONArray 파싱부터
+            val sides = json.getJSONArray("sides")
+            for (i in 0..sides.length()-1){
+                val side = sides.getJSONObject(i)
+            }
+
             return topic
         }
     }
---
+++ app/src/main/java/com/hgney/apipractice_20200615/datas/TopicSide.kt
@@ -0,0 +1,25 @@
+package com.hgney.apipractice_20200615.datas
+
+import org.json.JSONObject
+
+//토론에서 선택 가능 진영 정보 저장 클래스
+class TopicSide {
+
+    var id = 0
+    var TopicId = 0
+    var title = ""
+    var voteCount = 0
+
+//    json 덩어리가 input => 내용이 모두 적힌 TopicSide 객체 output
+    companion object{
+        fun getTopicSideFromJson(json: JSONObject) : TopicSide {
+            val ts = TopicSide()
+            ts.id = json.getInt("id")
+            ts.TopicId = json.getInt("topic_id")
+            ts.title = json.getString("title")
+            ts.voteCount = json.getInt("voteCount")
+            return ts
+        }
+    }
+
+}
